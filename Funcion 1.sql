d231015a58978549b5e4c76c605ff69efa617143
LucaTamasi <48994432+LucaTamasi@users.noreply.github.com>
2024-02-27 20:04:01 -0300

    Delete Funcion 1.sql

--- Funcion 1.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `f_cant_gen`(bgenero char(20)) RETURNS int
-    NO SQL
-BEGIN
-    DECLARE resultado int;
-    SELECT count(*) into resultado FROM cancion where genero = bgenero;
-    RETURN resultado;
-END
